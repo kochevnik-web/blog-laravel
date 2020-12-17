aef5212057386802f521eb9eb1d2b17ce5c9b562
Kovalev Dmitriy <kacevnik@yandex.ru>
2020-12-17 20:40:04 +0300

    routing app

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,22 @@
 |
 */
 
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// Route::get('/', function () {
+//     return '<h1>Hello, World!</h1>';
+// });
+
 Route::get('/', function () {
-    return view('welcome');
+    $var  = 3 + 2;
+    $name = 'John';
+    // return view('home')->with('result', $var);
+    // return view('home', ['result' => $var, 'name' => $name]);
+    return view('home', compact('var', 'name'));
+});
+
+Route::get('/about', function(){
+    return '<h1>About Page</h1>';
 });
