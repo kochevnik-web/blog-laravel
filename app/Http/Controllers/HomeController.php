83ea9c9f96949569f98f2fe91bb90d82bcc3520a
Kovalev Dmitriy <kacevnik@yandex.ru>
2020-12-22 16:57:09 +0300

    create models country and cites

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,16 +3,19 @@
 namespace App\Http\Controllers;
 
 use App\Post;
+use App\Country;
+use App\City;
 
 class HomeController extends Controller
 {
     public function index()
     {
+        // $data = Country::all();
+        // $data = Country::where('Code', '<', 'ALB')->select('Code', 'Name')->offset(1)->limit(2)->get();
+        // $data = City::find(5);
+        $data = Country::find('AGO');
 
-        $post = new Post;
-        $post->title = 'Статья 1';
-        $post->content = 'Контент статьи 1';
-        $post->save();
+        dd($data);
 
         return view('home', ['var' => 5, 'name' => 'John']);
     }
