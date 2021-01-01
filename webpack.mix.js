ebc9ac968a34bdd9b6b7f08ec08bbbd5d77bc15d
Kovalev Dmitriy <kacevnik@yandex.ru>
2021-01-01 23:36:07 +0300

    front mix 1

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,14 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+mix.styles([
+    'resources/src/css/bootstrap.css',
+    'resources/src/css/style.css',
+], 'public/assets/css/bundle.css');
+
+mix.scripts([
+    'resources/src/js/jquery.js',
+    'resources/src/js/bootstrap.js',
+], 'public/assets/js/bundle.js');
+
+// mix.copyDirectory('resources/src/img', 'public/assets/img');
