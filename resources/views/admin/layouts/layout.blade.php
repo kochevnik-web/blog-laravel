a118588e9d43fd534b9b10f8bdea9518a2976827
Kovalev Dmitriy <kacevnik@yandex.ru>
2021-01-16 14:22:04 +0300

    admin panel

--- resources/views/admin/layouts/layout.blade.php
+++ resources/views/admin/layouts/layout.blade.php
@@ -138,12 +138,12 @@
   <!-- Main Sidebar Container -->
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="../../index3.html" class="brand-link">
+    <a href="{{ url('/') }}" target="blank" class="brand-link">
       <img src="{{ asset('assets/admin/img/AdminLTELogo.png') }}"
            alt="AdminLTE Logo"
            class="brand-image img-circle elevation-3"
            style="opacity: .8">
-      <span class="brand-text font-weight-light">AdminLTE 3</span>
+      <span class="brand-text font-weight-light">На сайт</span>
     </a>
 
     <!-- Sidebar -->
@@ -165,7 +165,7 @@
                with font-awesome or any other icon font library -->
           <li class="nav-item">
             <a href="{{ route('index') }}" class="nav-link">
-              <i class="nav-icon far fa-home"></i>
+              <i class="nav-icon fas fa-home"></i>
               <p>Главная</p>
             </a>
           </li>
@@ -206,55 +206,7 @@
     <!-- /.sidebar -->
   </aside>
 
-  <!-- Content Wrapper. Contains page content -->
-  <div class="content-wrapper">
-    <!-- Content Header (Page header) -->
-    <section class="content-header">
-      <div class="container-fluid">
-        <div class="row mb-2">
-          <div class="col-sm-6">
-            <h1>Blank Page</h1>
-          </div>
-          <div class="col-sm-6">
-            <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="#">Home</a></li>
-              <li class="breadcrumb-item active">Blank Page</li>
-            </ol>
-          </div>
-        </div>
-      </div><!-- /.container-fluid -->
-    </section>
-
-    <!-- Main content -->
-    <section class="content">
-
-      <!-- Default box -->
-      <div class="card">
-        <div class="card-header">
-          <h3 class="card-title">Title</h3>
-
-          <div class="card-tools">
-            <button type="button" class="btn btn-tool" data-card-widget="collapse" data-toggle="tooltip" title="Collapse">
-              <i class="fas fa-minus"></i></button>
-            <button type="button" class="btn btn-tool" data-card-widget="remove" data-toggle="tooltip" title="Remove">
-              <i class="fas fa-times"></i></button>
-          </div>
-        </div>
-        <div class="card-body">
-          Start creating your amazing application!
-        </div>
-        <!-- /.card-body -->
-        <div class="card-footer">
-          Footer
-        </div>
-        <!-- /.card-footer-->
-      </div>
-      <!-- /.card -->
-
-    </section>
-    <!-- /.content -->
-  </div>
-  <!-- /.content-wrapper -->
+    @yield('content')
 
   <footer class="main-footer">
     <div class="float-right d-none d-sm-block">
