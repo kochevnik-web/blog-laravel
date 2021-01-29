831615c8a1a69b890f8e6a7486849321d4b01e12
Kovalev Dmitriy <kacevnik@yandex.ru>
2021-01-29 23:26:56 +0300

    change pagination

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -15,7 +15,7 @@ class CategoryController extends Controller
      */
     public function index()
     {
-        $categories = Category::paginate(20);
+        $categories = Category::paginate(10);
         return view('admin.categories.index', compact('categories'));
     }
 
